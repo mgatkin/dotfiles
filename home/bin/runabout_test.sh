025a488aac51129ed00984b5bbbdf96387275b87
Mark Atkin <mgatkin@gmail.com>
2018-11-12 12:06:44 -0500

    Fix typo in runabout_test.sh.

--- home/bin/runabout_test.sh
+++ home/bin/runabout_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-ssh-add -l | grep "runabout_test"
-if [ $# -ne 0 ]; then
+ssh-add -l | grep "runabout_test" > /dev/null
+if [ $? -ne 0 ]; then
     ssh-add ~/.ssh/runabout_test.id_rsa
 fi
 ssh runabout_test
